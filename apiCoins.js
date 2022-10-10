281d2d2a44beacea56d1b0e3407a8c67b640ef7a
AlexandreRocha-10 <alexandre.e.rocha@gmail.com>
2022-10-10 13:17:21 -0300

    Requisitos completos

--- apiCoins.js
+++ apiCoins.js
@@ -1,4 +1,25 @@
 const fetchCoins = async () => {
+    try {
+        const response = await fetch('https://api.coincap.io/v2/assets');
+        const obj = await response.json();
+        const { data } = obj;
+        return data.slice(0, 10);
+    } catch (error) {
+        console.log('Error', error);
+    }
 };
 
-fetchCoins();
+const setCoins = async () => {
+    const coins = await fetchCoins();
+    const coinsList = document.getElementById('coins-list');
+    coinsList.innerHTML = '';
+    coins.forEach((coin) => {
+      const li = document.createElement('li');
+      li.innerText = `${coin.name} (${coin.symbol}): ${Number(coin.priceUsd).toFixed(2)}`;
+      coinsList.appendChild(li);
+    });
+};
+
+window.onload = async () => {
+    await setCoins();
+};
